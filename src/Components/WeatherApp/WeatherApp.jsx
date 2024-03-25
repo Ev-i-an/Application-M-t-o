e8b86fe68159834bc00288422647d53fd6d36507
mouganib <mouganib@gmail.com>
2024-03-25 13:03:48 +0100

    80% de fait il me reste juste à changer l'url

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -12,6 +12,31 @@ import humidity_icon from '../Assets/humidity.png';
 
 
  const WeatherApp = () => {
+
+    let api_key = "49a8decfad8ca07d1b0f4de3af6461b1"; // Openweathermap API key
+
+    const search = async () => {
+        const element = document.getElementsByClassName("cityInput")
+        if(element[0].value === ""){
+            return 0;
+        }
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?d=${element[0].value}&units=Metric&lang=fr&appid=${api_key}`
+
+        let response = await fetch(url);
+        let data = await response.json();
+        const humidity =document.getElementsByClassName("humidity-percent")
+        const wind = document.getElementsByClassName("wind-rate")
+        const temperature = document.getElementsByClassName("weather-temp")
+        const location = document.getElementsByClassName("weather-location")
+
+        humidity[0].innerHTML= data.main.humidity+" %";
+        wind[0].innerHTML= data.wind.speed+" km/h";
+        temperature[0].innerHTML= data.main.temp+"°c";
+        location[0].innerHTML= data.name;
+    }
+
+    
   return (
     <div className='container'>
         <div className="top-bar">
@@ -36,7 +61,7 @@ import humidity_icon from '../Assets/humidity.png';
             <div className="element">
                 <img src={wind_icon} alt="" className='icon'/>
                 <div className="data">
-                    <div className="humidity-percent">18 kn/h</div>
+                    <div className="wind-rate">18 kn/h</div>
                     <div className="text">Vitesse du vent</div>
                 </div>
             </div>
